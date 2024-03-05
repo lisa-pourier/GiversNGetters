e6e91ccd328c447a28634841b0b3ad511b90f953
dogusakyuz <av.dogusakyuz@gmail.com>
2024-03-05 12:43:29 +0100

    Add dotenv-rails

---
+++ app/controllers/user_expertises_controller.rb
@@ -0,0 +1,2 @@
+class UserExpertisesController < ApplicationController
+end
---
+++ test/controllers/expertises_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ExpertisesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
