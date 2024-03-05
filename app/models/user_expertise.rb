98e54d8b09a6302fd66da54e7b4cd092fa5bd67f
Roberto Ferrara <robyorke95@gmail.com>
2024-03-05 13:01:51 +0100

    models_setup

---
+++ app/models/user_expertise.rb
@@ -0,0 +1,2 @@
+class UserExpertise < ApplicationRecord
+end
---
+++ test/models/user_expertise_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class UserExpertiseTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
