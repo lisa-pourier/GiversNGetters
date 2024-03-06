d6f5b1599a43a91555a4b556e4ddb9a9a1cf12e2
lisa pourier <lisapourier@gmail.com>
2024-03-06 17:24:59 +0100

    usersindexpage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index # aka users: this is the method that will be called when the user goes to the /users page
+    @users = User.all
+    
+  end
+
+  def show # aka user: this is the method that will be called when the user goes to the /users/:id page
+    @user = User.find(params[:id])
+  end
 end
