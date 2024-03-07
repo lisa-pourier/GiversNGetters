b1dd2ff1a727783c83730e0bba84473c5cc98888
dogusakyuz <av.dogusakyuz@gmail.com>
2024-03-07 16:35:11 +0100

    Requests Index created

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,6 +3,10 @@ class RequestsController < ApplicationController
     @requests = Request.all
   end
 
+  def show
+    @request = Request.find(params[:id])
+  end
+
   def new
     @request = Request.new
   end
