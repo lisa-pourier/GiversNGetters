f30c2b23f33cd4101e52f32ce6b3ffe970f5ee9e
dogusakyuz <av.dogusakyuz@gmail.com>
2024-03-14 12:27:26 +0100

    Changes on Message Page

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,7 @@
 class ChatsController < ApplicationController
+  def show
+    @sender = User.find(params[:sender_id])
+    @receiver = User.find(params[:receiver_id])
+    @messages = Message.where(sender: [@sender, @receiver], receiver: [@sender, @receiver]).order(created_at: :asc)
+  end
 end
